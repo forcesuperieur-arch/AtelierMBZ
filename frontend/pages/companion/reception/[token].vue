14605e1313cd1caea78c401d9749d2d7d29755d0
cmoreau <cmoreau@d3t.lan>
2026-04-24 12:17:00 +0200

    fix: restaure wrappers companion supprimés par erreur dans LOT-0
    Les wrappers pages/companion/reception/[token].vue et
    pages/companion/vo/[token].vue ont été supprimés dans LOT-0
    ('code mort') alors qu'ils sont utilisés par le planning
    pour générer les URLs du compagnon PDA.
    
    Restauration des 2 wrappers qui importent les pages publiques
    (public/companion/[token].vue et public/vo-companion/[token].vue)
    et désactivent le layout pour affichage plein écran.

---
+++ frontend/pages/companion/reception/[token].vue
@@ -0,0 +1,9 @@
+<template>
+  <ReceptionCompanionPage />
+</template>
+
+<script setup lang="ts">
+import ReceptionCompanionPage from '../../public/companion/[token].vue'
+
+definePageMeta({ layout: false })
+</script>
---
+++ frontend/pages/companion/vo/[token].vue
@@ -0,0 +1,9 @@
+<template>
+  <VoCompanionPage />
+</template>
+
+<script setup lang="ts">
+import VoCompanionPage from '../../public/vo-companion/[token].vue'
+
+definePageMeta({ layout: false })
+</script>
